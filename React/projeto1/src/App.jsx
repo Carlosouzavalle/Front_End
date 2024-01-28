6a9baab5332f58880ac3ed882209fa56e5d04b31
Carlosouzavalle <carlosouzavalle@gmail.com>
2024-01-27 23:25:57 -0300

    um pouco mais de react

--- React/projeto1/src/App.jsx
+++ React/projeto1/src/App.jsx
@@ -1,17 +1,18 @@
 import Titulo from "./Titulo"
 
 function App() {
-  let nome =  'carlos'
-  const urlimg = "https://i.pinimg.com/originals/1a/8a/97/1a8a97fb3f91d89ad5fcf7e1de523074.gif"
+  
+  // const urlimg = "https://i.pinimg.com/originals/1a/8a/97/1a8a97fb3f91d89ad5fcf7e1de523074.gif"
 
 
 
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>Olá sou o {nome}</p>
-      <img src={urlimg}/>
-      < Titulo/>
+      {/* <img src={urlimg}/> */}
+      < Titulo cor="red" nome="Carlos" paragrafo={true}/>
+      < Titulo cor="blue"/>
+      < Titulo cor="orange"/>
     </div>
   )
 }
--- React/projeto1/src/Titulo.jsx
+++ React/projeto1/src/Titulo.jsx
@@ -1,5 +1,15 @@
-function Titulo() {
-    return <h1>Titulo</h1>
+function Titulo({nome, paragrafo, cor}) {
+    return (
+        <div>
+            <h1 style={{color: cor}}> Oi eu sou {nome ? nome : "Fulano"}</h1>
+            { paragrafo
+                ?
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, excepturi!</p>
+                :
+                <p>Nada</p>
+            }
+        </div>
+    )
 }
 
 export default Titulo
